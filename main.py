624f876d0a93db5948372311aba8351f1ee315f0
Geo <63842114+amm0nition@users.noreply.github.com>
2025-08-23 14:03:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,6 @@ import sys
 import mysql.connector
 from mysql.connector import Error
 
-# try:
-#     connection = mysql.connector.connect(
-#         host='localhost',
-#         user='libra_user',
-#         password='libra_pass',
-#         database='libratrack_db'
-#     )
-
-#     if connection.is_connected():
-#         print("Connection successful!")
-#         db_info = connection.server_info
-#         print("MariaDB/MySQL server version:", db_info)
-
-# except Error as e:
-#     print("Error while connecting:", e)
-
-# finally:
-#     if 'connection' in locals() and connection.is_connected():
-#         connection.close()
-#         print("Connection closed.")
-
 def main():
     print("Select Menu:\n1. View Library Books\n2. View Patron List\n3. Exit.")
     select_menu = input("Enter the number: ")
